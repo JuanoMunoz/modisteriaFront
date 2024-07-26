6decdb6474181b72faca9960356e30729449b34b
Juano <juanjosemunozva@gmail.com>
2024-07-25 21:25:23 -0500

    adding principal font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
-  return <Button content={"presiona"} color={"warning"} />;
+  return (
+    <>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum vitae
+        maxime nesciunt necessitatibus eveniet <i>placeat dolore, ratione ea</i>
+        <span style={{ fontWeight: 900, fontStyle: "italic" }}>
+          s laboriosam perferendis sed ipsa unde illum possimus repudiandae?
+        </span>{" "}
+        Vel deserunt dolore ad. born under a bad sign with a blue moon in your
+        eyes
+      </p>
+    </>
+  );
 }
 
 export default App;
