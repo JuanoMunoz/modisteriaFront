d41f25d27c6d31b511404ddb7e4450cc39a8cafa
PatitoDestructor <cristianvelez308@gmail.com>
2024-08-08 15:21:32 -0500

    saa

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,57 +1,66 @@
 import Metadata from "../../components/metadata/Metadata";
 import "./register.css";
+import video from "/registro.mp4"
 import { useForm } from "react-hook-form";
 export default function Register() {
-  const EMAIL_REGEX =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-  console.log(watch("name"));
+  // const EMAIL_REGEX =
+  //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   watch,
+  //   formState: { errors },
+  // } = useForm();
+  // const onSubmit = (data) => {
+  //   console.log(data);
+  // };
+  // console.log(watch("name"));
 
   return (
     <>
       <Metadata title={"Registro - Modistería Doña Luz"}></Metadata>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className="form">
-        <section>
-          <article>
-            <label htmlFor="">
-              <h4>Nombre</h4>
-              <input {...register("name", { required: true })} type="text" />
-              {errors.name?.type === "required" && <h6>la rompiste sapo</h6>}
-            </label>
-            <label htmlFor="">
-              <h4>Email</h4>
-              <input
-                type="email"
-                {...register("email", { required: true, pattern: EMAIL_REGEX })}
-              />
-              {errors.name?.type === "required" && <h6>Es requerido sapo</h6>}
-              {errors.name?.type === "pattern" && (
-                <h6>No es un correo válido gonorriento</h6>
-              )}
-            </label>
-          </article>
-          <article>
-            <label htmlFor="">
-              <h4>Teléfono</h4>
-              <input type="tel" name="" id="" />
-            </label>
-            <label htmlFor="">
-              <h4>Contraseña</h4>
-              <input type="password" name="" id="" />
-            </label>
-          </article>
-        </section>
-        <button className="boton-submit">crear cuenta</button>
-      </form>
+      <h1>Registrate</h1>
+
+      <div className="contenedor">
+        <div className="imagen">
+            <video src={video} autoPlay loop muted className="video"></video>
+        </div>
+        <div className="form">
+            <form action="#">
+
+              <div className="input-container">
+                <input type="text" className="input-field"/>
+                <label className="input-label">Nombre</label>
+                <span className="input-highlight"></span>
+              </div>
+
+              <div className="input-container">
+                <input type="text" className="input-field"/>
+                <label className="input-label">Télefono</label>
+                <span className="input-highlight"></span>
+              </div>
+
+              <div className="input-container">
+                <input type="text" className="input-field"/>
+                <label className="input-label">Correo</label>
+                <span className="input-highlight"></span>
+              </div>
+
+              <div className="input-container">
+                <input type="password" className="input-field"/>
+                <label className="input-label">Contraseña</label>
+                <span className="input-highlight"></span>
+              </div>
+
+              <div className="input-container">
+                <input type="password" className="input-field"/>
+                <label className="input-label">Repetir Contraseña</label>
+                <span className="input-highlight"></span>
+              </div>
+            </form>
+        </div>
+      </div>
+      
     </>
   );
 }
