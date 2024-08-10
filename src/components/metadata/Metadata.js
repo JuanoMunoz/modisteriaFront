43288c477a66f8a4c2c0e61c19fee742c26853bd
Juano <juanjosemunozva@gmail.com>
2024-08-09 21:54:27 -0500

    añadiendo validaciones a input, sin estilos

--- src/components/metadata/Metadata.js
+++ src/components/metadata/Metadata.js
@@ -1,9 +1,6 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
 export default function Metadata({ title }) {
-    const location = useLocation()
-    console.log(location)
+
     useEffect(() => {
         document.title = title || 'Modistería Doña Luz'
     },[title])
