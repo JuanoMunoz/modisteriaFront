8198eab622d522de2bf1231b5810c98f9e1bd8e3
Juano <juanjosemunozva@gmail.com>
2024-09-05 18:26:57 -0500

    carrito layout

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Register from "./pages/register/Register";
 import LayoutDashboard from "./components/layoutManuela/LayoutDashboard";
 import Estadisticas from "./pages/estadisticas/Estadisticas";
 import InicioSesion from "./pages/sesion/Sesion";
-import Catalogo from "./pages/catalogo/Catalogo"
+import Catalogo from "./pages/catalogo/Catalogo";
+import Citas from "./pages/Citas/Citas";
 function App() {
   return (
     <>
@@ -15,11 +16,12 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
           <Route path="/registro" element={<Register />}></Route>
-          <Route path="/sesion" element={<InicioSesion/>}></Route>
-          <Route path="/catalogo" element={<Catalogo/>}></Route>y
+          <Route path="/sesion" element={<InicioSesion />}></Route>
+          <Route path="/cita" element={<Citas />}></Route>
+          <Route path="/catalogo" element={<Catalogo />}></Route>y
         </Route>
-        <Route path="/dashboard" element={<LayoutDashboard/>}>
-          <Route index element={<Estadisticas/>}></Route>
+        <Route path="/dashboard" element={<LayoutDashboard />}>
+          <Route index element={<Estadisticas />}></Route>
         </Route>
         <Route path="*" element={<Error404 />}></Route>
       </Routes>
